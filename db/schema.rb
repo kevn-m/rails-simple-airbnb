62b89402de36df5bf10e73593adca6136e35fbee
kjam0 <south.kay@gmail.com>
2021-06-23 17:59:37 +1000

    db switch

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,19 @@
 
 ActiveRecord::Schema.define(version: 2021_06_23_053943) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+  end
+
   create_table "flats", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -22,4 +35,21 @@ ActiveRecord::Schema.define(version: 2021_06_23_053943) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "overview"
+    t.string "poster_url"
+    t.float "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
