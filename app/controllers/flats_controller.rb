f9283ae82b1bbc87b8accd8afd5377cc9328aa1e
kjam0 <south.kay@gmail.com>
2021-06-23 17:46:22 +1000

    new and show actions

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -8,5 +8,19 @@ class FlatsController < ApplicationController
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
